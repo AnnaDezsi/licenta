1b071d20384b4e1d9ab2a3613596879567bf6eab
Anna Alexa Dezsi <dezsianna15@yahoo.com>
2024-11-30 13:04:46 +0200

    TSK-17: text parser done

---
+++ UI/src/mockup_data/parserTest.js
@@ -0,0 +1,14 @@
+export const parserMockupTextExample = `
+# Titlu Principal h1
+## Titlu Secundar h2
+### Titlu Secundar h3
+#### Titlu Secundar h4
+##### Titlu Secundar h5
+###### Titlu Secundar h6
+Acesta este un paragraf cu **text îngroșat** și o linie nouă.
+
+- Primul element de listă
+- Al doilea element de listă
+
+Un alt paragraf simplu.
+`;
--- UI/src/pages/dashboard.jsx
+++ UI/src/pages/dashboard.jsx
@@ -1,7 +1,11 @@
+import { TextParser } from "../components/TextParser/TextParser"
+import { parserMockupTextExample } from "../mockup_data/parserTest"
 
 export const Dashboard = () => {
+
   return (
     <div>
+      <TextParser text={parserMockupTextExample}/>
     </div>
   )
 }
