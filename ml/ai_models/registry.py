25d9bae261a410cc1a938c3b5278e50a317ac60a
Anna <annadezsi15@yahoo.com>
2025-07-05 15:38:45 +0300

    save progress 3

--- ml/ai_models/registry.py
+++ ml/ai_models/registry.py
@@ -1,13 +1,8 @@
-from ai_models.diabetes_model import DiabetesModel
 from ai_models.liver_model import LiverDiseaseModel
 from ai_models.breast_cancer_model import BreastCancerModel
 
 def load_models():
     models = []
-
-    diabetes = DiabetesModel()
-    diabetes.load()
-    models.append(diabetes)
 
     liver = LiverDiseaseModel()
     liver.load()
